8909b7f6cf1f6ac0e4bd838ff6dc499c7f4b8bfa
sumiya19 <tajnsy@gmail.com>
2018-12-19 11:44:34 +0900

    ユーザー詳細画面とフォロー機能を追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update, :destroy, :like, :unlike]
+	before_action :authenticate_user!, except: [:index, :show, :destroy, :like, :unlike , :following, :followers]
+
+	def show
+	end
+
+	private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:user)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tweeets#index"
   resources :relationships,       only: [:create, :destroy]
+  resources :users
 
 end
