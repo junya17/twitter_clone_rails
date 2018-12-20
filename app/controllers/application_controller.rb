617d43c17d9dfab653575e265804abab772a5530
sumiya19 <tajnsy@gmail.com>
2018-12-20 22:37:49 +0900

    navbarデザイン修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-	before_action :detect_device
-
-	private
-	def detect_device
-		case request.user_agent
-		when 'mobile'
-			request.variant = :mobile
-		when 'tablet'
-			request.variant = :tablet
-		end
-	end
 end
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 require 'net/http'
 require 'uri'
 require 'json'
 
 class NewsController < ApplicationController
-	def index
-		uri = URI.parse('https://newsapi.org/v2/top-headlines?country=jp&apiKey=bd52bc962510460eb540907e53f5acd4')
-		json = Net::HTTP.get(uri)
-		result = JSON.parse(json)
-		@newses = result['articles']
-	end
+  def index
+    uri = URI.parse('https://newsapi.org/v2/top-headlines?country=jp&apiKey=bd52bc962510460eb540907e53f5acd4')
+    json = Net::HTTP.get(uri)
+    result = JSON.parse(json)
+    @newses = result['articles']
+  end
 end
-
