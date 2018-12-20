af979752219f44c4d7e4e2ca98f4b0e6d04d498e
sumiya19 <tajnsy@gmail.com>
2018-12-20 18:54:46 +0900

    ニューススマホ対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+	before_action :detect_device
+
+	private
+	def detect_device
+		case params[:type]
+		when 'mobile'
+			request.variant = :mobile
+		when 'tablet'
+			request.variant = :tablet
+		end
+	end
 end
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,7 +4,7 @@ require 'json'
 
 class NewsController < ApplicationController
 	def index
-		uri = URI.parse('https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=bd52bc962510460eb540907e53f5acd4')
+		uri = URI.parse('https://newsapi.org/v2/top-headlines?country=jp&apiKey=bd52bc962510460eb540907e53f5acd4')
 		json = Net::HTTP.get(uri)
 		result = JSON.parse(json)
 		@newses = result['articles']
